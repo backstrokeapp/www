94f7a54b7a6ce0535b1459b71d542e03f5dfb564
Ryan Gaus <rsg1egoman@gmail.com>
2017-10-14 16:15:02 -0400

    chore(mixpanel): track when users click login

--- src/components/header-login-button/index.js
+++ src/components/header-login-button/index.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function HeaderLoginButton({text}) {
   return <div className="header-login-section">
     <img className="header-login-button-man" role="presentational" src="/assets/img/login_button_man.png" />
-    <a href="https://app.backstroke.co" className="header-login-button">
+    <a href="https://app.backstroke.co" className="header-login-button" id="header-login-button">
       {text || 'Login with Github'}
     </a>
   </div>;
