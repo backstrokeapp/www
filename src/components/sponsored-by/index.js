89c1da8dbc9fff0c77c3f0761e2b611782d6f4ff
Matthew Selby <matthewcselby@icloud.com>
2018-01-22 16:17:24 -0800

    change digitalocean logo to local asset

--- src/components/sponsored-by/index.js
+++ src/components/sponsored-by/index.js
@@ -5,7 +5,7 @@ export default function SponsoredBy() {
     <h3 className="sponsored-by-header">Sponsored by</h3>
     <a className="sponsored-by-image" href="https://m.do.co/c/473e904fbb85">
       <img
-        src="https://www.digitalocean.com/assets/media/logos-badges/png/DO_Logo_Horizontal_Black-a93a7c21.png"
+        src="/assets/img/digitalocean.png"
         alt="Digitalocean"
       />
     </a>
